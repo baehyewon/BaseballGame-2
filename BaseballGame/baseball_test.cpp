215c30c1824347a643ee028fda1507022013284b
haewon <baehw1107@naver.com>
2025-07-02 14:35:28 +0900

    Add 1 Stirkes, 2 Ball test

--- BaseballGame/baseball_test.cpp
+++ BaseballGame/baseball_test.cpp
@@ -38,6 +38,13 @@ TEST_F(BaseballFixture, Return2Strikes0Ball) {
 	EXPECT_EQ(2, result.strikes);
 	EXPECT_EQ(0, result.balls);
 }
+TEST_F(BaseballFixture, Return1Strikes2Ball) {
+	GuessResult result = game.guess("132");
+
+	EXPECT_FALSE(result.solved);
+	EXPECT_EQ(1, result.strikes);
+	EXPECT_EQ(2, result.balls);
+}
 
 int main() {
 	InitGoogleMock();
